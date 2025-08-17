b8d17ca3fbe33f94ae52c5350e759a1eb7284c6f
Maz <mazoolander@me.com>
2025-08-17 16:39:24 +0200

    💄 Add gradient and image assets for testimonial UI

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,16 +1,21 @@
+import prevIcon from "../assets/icon-prev.svg";
+import nextIcon from "../assets/icon-next.svg";
+
 const Testimonial = ({ testimonial, onNext, onPrev }) => {
   return (
     <>
-      <h2>{testimonial.name}</h2>
-      <blockquote>{testimonial.quote}</blockquote>
-      <p>{testimonial.role}</p>
-      <img src={testimonial.image} alt={`Photo of ${testimonial.name}`} />
-      <button onClick={onPrev} className="btn_Prev">
-        Prev
-      </button>
-      <button onClick={onNext} className="btn_Next">
-        Next
-      </button>
+      <div>
+        <img src={testimonial.image} alt={`Photo of ${testimonial.name}`} />
+        <blockquote>{testimonial.quote}</blockquote>
+        <h2>{testimonial.name}</h2>
+        <p>{testimonial.role}</p>
+        <button onClick={onPrev} className="btn_Prev">
+          <img src={prevIcon} alt="Previous" />
+        </button>
+        <button onClick={onNext} className="btn_Next">
+          <img src={nextIcon} alt="Next" />
+        </button>{" "}
+      </div>
     </>
   );
 };
