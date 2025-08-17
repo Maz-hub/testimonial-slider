0f18e9fb4e10725c005f87348829af8a81848794
Maz <mazoolander@me.com>
2025-08-17 18:08:04 +0200

    Align author name and title side-by-side with flex layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,13 @@ import Testimonial from "./components/Testimonial.jsx";
 
 const testimonalData = [
   {
-    quote:
-      "I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future.",
+    quote: `"I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future."`,
     name: "Tanya Sinclair",
     role: "UX Engineer",
     image: tanyaImg,
   },
   {
-    quote:
-      "If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.",
+    quote: `"If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer."`,
     name: "John Tarkpor",
     role: "Junior Front-end Developer",
     image: johnImg,
@@ -31,10 +29,10 @@ function App() {
       setCurrentIndex((prev) => prev + 1);
     }
   };
-// button decrement the currentIndex
+  // button decrement the currentIndex
   const handlePrev = () => {
     if (currentIndex === 0) {
-      setCurrentIndex(testimonalData.length - 1)
+      setCurrentIndex(testimonalData.length - 1);
     } else {
       setCurrentIndex((prev) => prev - 1);
     }
@@ -42,9 +40,10 @@ function App() {
 
   return (
     <>
-      <Testimonial testimonial={testimonalData[currentIndex]}
-      onNext={handleNext}
-      onPrev={handlePrev}
+      <Testimonial
+        testimonial={testimonalData[currentIndex]}
+        onNext={handleNext}
+        onPrev={handlePrev}
       />
     </>
   );
--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,20 +1,47 @@
 import prevIcon from "../assets/icon-prev.svg";
 import nextIcon from "../assets/icon-next.svg";
+import quoteIcon from "../assets/pattern-quotes.svg";
+import patternBg from "../assets/pattern-bg.svg";
+import patternCurve from "../assets/pattern-curve.svg";
 
 const Testimonial = ({ testimonial, onNext, onPrev }) => {
   return (
     <>
-      <div>
-        <img src={testimonial.image} alt={`Photo of ${testimonial.name}`} />
-        <blockquote>{testimonial.quote}</blockquote>
-        <h2>{testimonial.name}</h2>
-        <p>{testimonial.role}</p>
-        <button onClick={onPrev} className="btn_Prev">
-          <img src={prevIcon} alt="Previous" />
-        </button>
-        <button onClick={onNext} className="btn_Next">
-          <img src={nextIcon} alt="Next" />
-        </button>{" "}
+      <div className="testimonial">
+        {/* Left side / Top */}
+        <div className="testimonial__content">
+          <img src={quoteIcon} alt="Qoute Icon" />
+          <blockquote className="testimonial__text">
+            {testimonial.quote}
+          </blockquote>
+          <div className="testimonial__author">
+            <p className="testimonial__name">{testimonial.name}</p>
+            <p className="testimonial__title">{testimonial.role}</p>
+          </div>
+        </div>
+        {/* Right side / Bottom */}
+        <div className="testimonial__image-container">
+          <img
+            className="testimonial__image"
+            src={testimonial.image}
+            alt={`Photo of ${testimonial.name}`}
+          />
+          <img src={patternBg} alt="" className="testimonial__pattern-bg" />
+          <img
+            src={patternCurve}
+            alt=""
+            className="testimonial__pattern-curve"
+          />
+        </div>
+        {/* Buttons */}
+        <div className="testimonial__nav">
+          <button onClick={onPrev} className="btn_Prev">
+            <img src={prevIcon} alt="Previous" />
+          </button>
+          <button onClick={onNext} className="btn_Next">
+            <img src={nextIcon} alt="Next" />
+          </button>
+        </div>
       </div>
     </>
   );
